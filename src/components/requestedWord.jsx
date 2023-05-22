999529d0267c30be934fcc26552c6d6a7f5de90e
MohammadHajMahmoud <mohammdhasaeen@yahoo.com>
2023-05-22 09:06:39 +0300

    style

--- src/components/requestedWord.jsx
+++ src/components/requestedWord.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useRef } from 'react';
 import "./cssFile/availableWords.css";
 import { Link } from "react-router-dom";
 function RequestedWord({ word }) {
+  const liRef = useRef(null);
+
+  const handleClick = () => {
+    const linkElement = liRef.current.querySelector('.link');
+    if (linkElement) {
+      linkElement.click();
+    }
+  };
   return (
-    <li>
-      <Link className="link" to={`/upload-video/${word}`}>{word}</Link>
+    <li ref={liRef} onClick={handleClick} className="link-li">
+      <Link className="link"  to={`/upload-video/${word}`}>{word}</Link>
     </li>
   );
 }
